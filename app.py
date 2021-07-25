476cfbf7617dc55982c5a8ca91776d7a6bd11a4b
RohanPhadnis <59270766+RohanPhadnis@users.noreply.github.com>
2021-07-25 13:59:25 -0700

    API code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import os
 import json
 import bson
@@ -8,6 +9,8 @@ from flask_pymongo import *
 app = Flask('WebPosts')
 app.config['MONGO_URI'] = 'mongodb+srv://anyone:xyz@flask.ngjrl.mongodb.net/flask_react_db?retryWrites=true&w=majority'
 mongo = PyMongo(app)
+lock_time = 60 * 15
+encoder = json.JSONEncoder()
 
 
 @app.route('/', methods=['GET'])
@@ -16,37 +19,145 @@ def home():
         return render_template('home.html')
 
 
-@app.route('/delete/<identity>', methods=['GET'])
-def delete(identity):
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
     if request.method == 'GET':
-        mongo.db.entries.delete_one({'_id': bson.ObjectId(identity)})
-        return redirect('/view')
-
-
-@app.route('/view', methods=['GET'])
-def view():
-    if request.method == 'GET':
-        docs = list(mongo.db.entries.find({}))
-        return render_template('view.html', docs=docs)
-
-
-@app.route('/json', methods=['GET'])
-def api():
-    if request.method == 'GET':
-        encoder = json.JSONEncoder()
-        docs = [{key: str(val) for key, val in doc.items()} for doc in mongo.db.entries.find({})]
-        docs = encoder.encode(docs)
-        return docs
-
-
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    if request.method == 'GET':
-        return render_template('post.html')
+        return render_template('reg.html')
     elif request.method == 'POST':
         resp = dict(request.form)
-        mongo.db.entries.insert_one(resp)
-        return redirect('/post')
+        if len(list(mongo.db.user.find({'user': resp['user']}))) > 0:
+            return render_template('reg.html', msg='username already exists')
+        else:
+            mongo.db.user.insert_one(resp)
+            resp['time'] = time.time()
+            mongo.db.session.insert_one(resp)
+            user_session = list(mongo.db.session.find({'user': resp['user']}))[0]
+            return redirect('/view/'+str(user_session['_id']))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        resp = dict(request.form)
+        user = list(mongo.db.user.find({'user': resp['user']}))
+        if len(user) == 0:
+            return render_template('login.html', msg='login does not exist')
+        else:
+            user = user[0]
+            if user['pass'] != resp['pass']:
+                return render_template('login.html', msg='incorrect password')
+            else:
+                resp['time'] = time.time()
+                mongo.db.session.insert_one(resp)
+                user_session = list(mongo.db.session.find({'user': resp['user']}))[0]
+                return redirect('/view/' + str(user_session['_id']))
+
+
+@app.route('/view/<user_session>', methods=['GET'])
+def view(user_session):
+    if request.method == 'GET':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))
+        if len(session) == 0:
+            return redirect('/login')
+        else:
+            session = session[0]
+            if time.time() - session['time'] >= lock_time:
+                mongo.db.session.delete_one({'_id': bson.ObjectId(user_session)})
+                return redirect('/login')
+            else:
+                docs = list(mongo.db.entry.find({'user': session['user']}))
+                return render_template('view.html', docs=docs, session=user_session)
+
+
+@app.route('/post/<user_session>', methods=['GET', 'POST'])
+def post(user_session):
+    if request.method == 'GET':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))
+        if len(session) == 0:
+            return redirect('/login')
+        else:
+            session = session[0]
+            if time.time() - session['time'] >= lock_time:
+                mongo.db.session.delete_one({'_id': bson.ObjectId(user_session)})
+                return redirect('/login')
+            else:
+                return render_template('post.html', session=user_session)
+    elif request.method == 'POST':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))[0]
+        resp = dict(request.form)
+        resp['user'] = session['user']
+        mongo.db.entry.insert_one(resp)
+        return redirect('/post/{}'.format(str(session['_id'])))
+
+
+@app.route('/loginapi', methods=['GET', 'POST'])
+def loginapi():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        resp = dict(request.form)
+        user = list(mongo.db.user.find({'user': resp['user']}))
+        if len(user) == 0:
+            return encoder.encode({'login': False, 'session': ''})
+        else:
+            user = user[0]
+            if user['pass'] != resp['pass']:
+                return encoder.encode({'login': False, 'session': ''})
+            else:
+                resp['time'] = time.time()
+                mongo.db.session.insert_one(resp)
+                user_session = list(mongo.db.session.find({'user': resp['user']}))[0]
+                return encoder.encode({'login': True, 'session': str(user_session)})
+
+
+@app.route('/api/<user_session>', methods=['GET'])
+def api(user_session):
+    if request.method == 'GET':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))
+        if len(session) == 0:
+            return encoder.encode(['please login'])
+        else:
+            session = session[0]
+            if time.time() - session['time'] >= lock_time:
+                mongo.db.session.delete_one({'_id': bson.ObjectId(user_session)})
+                return encoder.encode(['please login'])
+            else:
+                docs = list(mongo.db.entry.find({'user': session['user']}))
+                return encoder.encode(docs)
+
+
+@app.route('/delete/<user_session>/<entry_id>')
+def delete(user_session, entry_id):
+    if request.method == 'GET':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))
+        if len(session) == 0:
+            return redirect('/login')
+        else:
+            session = session[0]
+            if time.time() - session['time'] >= lock_time:
+                mongo.db.session.delete_one({'_id': bson.ObjectId(user_session)})
+                return redirect('/login')
+            else:
+                mongo.db.entry.delete_one({'_id': bson.ObjectId(entry_id)})
+                return redirect('/view/{}'.format(user_session))
+
+
+@app.route('/rdelete/<user_session>/<entry_id>')
+def rdelete(user_session, entry_id):
+    if request.method == 'GET':
+        session = list(mongo.db.session.find({'_id': bson.ObjectId(user_session)}))
+        if len(session) == 0:
+            return encoder.encode(['please login'])
+        else:
+            session = session[0]
+            if time.time() - session['time'] >= lock_time:
+                mongo.db.session.delete_one({'_id': bson.ObjectId(user_session)})
+                return encoder.encode(['please login'])
+            else:
+                mongo.db.entry.delete_one({'_id': bson.ObjectId(entry_id)})
+                return redirect('/api/{}'.format(user_session))
 
 
 if __name__ == '__main__':
